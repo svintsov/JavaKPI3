0d4884760d78ad362b8ba068c9e753019fc27806
bazyl <svintcov95@gmail.com>
2017-11-08 19:35:45 +0200

    properties files were created for better interlanguage experience

--- src/main/java/com/bazyl/preparing/controller/ValidatorTest.java
+++ src/main/java/com/bazyl/preparing/controller/ValidatorTest.java
@@ -6,77 +6,65 @@ import org.junit.Test;
 public class ValidatorTest {
     @Test
     public void isValidIndex() throws Exception {
-        Validator validator = new Validator();
-        Assert.assertTrue(validator.isValidIndex("02229"));
+        Assert.assertTrue(Validator.isValidIndex("02229"));
     }
 
     @Test
     public void isValidCity() throws Exception {
-        Validator validator = new Validator();
-        Assert.assertTrue(validator.isValidCity("New York"));
+        Assert.assertTrue(Validator.isValidCity("New York"));
     }
 
     @Test
     public void isValidStreet() throws Exception {
-        Validator validator = new Validator();
-        Assert.assertTrue(validator.isValidStreet("Gogol"));
+        Assert.assertTrue(Validator.isValidStreet("Gogol"));
     }
 
     @Test
     public void isValidHouse() throws Exception {
-        Validator validator = new Validator();
-        Assert.assertTrue(validator.isValidHouse("22b"));
+        Assert.assertTrue(Validator.isValidHouse("22b"));
     }
 
     @Test
     public void isValidApartment() throws Exception {
-        Validator validator = new Validator();
-        Assert.assertTrue(validator.isValidApartment("222"));
+        Assert.assertTrue(Validator.isValidApartment("222"));
     }
 
     @Test
     public void isValidSkype() throws Exception {
-        Validator validator = new Validator();
-        Assert.assertTrue(validator.isValidSkype("@hello"));
+        Assert.assertTrue(Validator.isValidSkype("@hello"));
     }
 
     @Test
     public void isValidEmail() throws Exception {
-        Validator validator = new Validator();
-        Assert.assertTrue(validator.isValidEmail("svintcov95@gmail.com"));
+        Assert.assertTrue(Validator.isValidEmail("svintcov95@gmail.com"));
     }
 
     @Test
     public void isValidPhone() throws Exception {
-        Validator validator = new Validator();
-        Assert.assertTrue(validator.isValidPhone("044 543 4766"));
+        Assert.assertTrue(Validator.isValidPhone("044 543 4766"));
     }
 
 
     @Test
     public void isValidGroup() throws Exception {
-        Validator validator = new Validator();
-        Assert.assertTrue(validator.isValidGroup("group1"));
+        Assert.assertTrue(Validator.isValidGroup("group1"));
     }
 
     @Test
     public void isValidComment() throws Exception {
-        Validator validator = new Validator();
-        Assert.assertTrue(validator.isValidComment("/*\n" +
+        Assert.assertTrue(Validator.isValidComment("/*\n" +
                 "*there\n" +
                 "*/"));
     }
 
     @Test
     public void isValidNickname() throws Exception {
-        Validator validator = new Validator();
-        Assert.assertTrue(validator.isValidNickname("h_"));
+        Assert.assertTrue(Validator.isValidNickname("h_"));
     }
 
     @org.junit.Test
     public void validateFIO() throws Exception {
-        Validator validator = new Validator();
-        Assert.assertTrue(validator.isValidFIO("hello"));
+        Assert.assertTrue(Validator.isValidFIO("hello"));
     }
 
 }
--- src/main/java/com/bazyl/preparing/view/Console.java
+++ src/main/java/com/bazyl/preparing/view/Console.java
@@ -1,27 +1,40 @@
 package com.bazyl.preparing.view;
 
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 
 public class Console {
 
-    public static String NAME_INPUT = "Name:";
-    public static String SURNAME_INPUT = "Surname:";
-    public static String SECONDNAME_INPUT = "Second name:";
-    public static String NICKNAME_INPUT = "Nickname:";
-    public static String COMMENT_INPUT = "Commentary:";
-    public static String GROUP_INPUT = "Group:";
-    public static String HOME_PHONE_INPUT = "Home phone:";
-    public static String MOBILE_PHONE_INPUT = "Mobile phone:";
-    public static String EMAIL_INPUT = "Email:";
-    public static String INDEX_INPUT = "Index:";
-    public static String SKYPE_INPUT = "Skype:";
-    public static String CITY_INPUT = "City:";
-    public static String STREET_INPUT = "Street:";
-    public static String HOUSE_INPUT = "House:";
-    public static String APPARTMENT_INPUT = "Appartment:";
+    static final String MESSAGES_BUNDLE_NAME = "messages";
+    static final ResourceBundle bundle =
+            ResourceBundle.getBundle(
+                    MESSAGES_BUNDLE_NAME,
+                    new Locale("ua"));  // Ukrainian
+    //new Locale("en"));        // English
 
+    public static String NAME_INPUT = "input.name";
+    public static String SURNAME_INPUT = "input.surname";
+    public static String SECONDNAME_INPUT = "input.secondname";
+    public static String NICKNAME_INPUT = "input.nickname";
+    public static String COMMENT_INPUT = "input.commentary";
+    public static String GROUP_INPUT = "input.group";
+    public static String HOME_PHONE_INPUT = "input.home_phone";
+    public static String MOBILE_PHONE_INPUT = "input.mobile_phone";
+    public static String EMAIL_INPUT = "input.mail";
+    public static String INDEX_INPUT = "input.index";
+    public static String SKYPE_INPUT = "input.skype";
+    public static String CITY_INPUT = "input.city";
+    public static String STREET_INPUT = "input.street";
+    public static String HOUSE_INPUT = "input.house";
+    public static String APPARTMENT_INPUT = "input.appartment";
 
-    public static void printMessage(String message){
-        System.out.println(message);
+    public static void printMessage(String message) {
+        try {
+            System.out.println(bundle.getString(message));
+        } catch (MissingResourceException MRE) {
+            System.out.println(message);
+        }
     }
 
 }
